0ffe4aad9d05361d59dd899ee823353458994a0f
Swarnalakshmi G B <53253072+laksmiswarna041@users.noreply.github.com>
2022-02-22 16:53:37 +0530

    Update pydoc.py

--- pydoc.py
+++ pydoc.py
@@ -1,8 +1,5 @@
 import requests,json
-response=requests.get('https://docs.python.org/3.8/library/pydoc')
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
+response=requests.get('https://docs.python.org/3.8/library/pydoc')#can get any input
+
 
 jprint(response.json())
