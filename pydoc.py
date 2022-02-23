657e1f771a39ec6221e57b04bd313bd04f0c12d5
Swarnalakshmi G B <53253072+laksmiswarna041@users.noreply.github.com>
2022-02-23 16:17:38 +0530

    Update pydoc.py

--- pydoc.py
+++ pydoc.py
@@ -1,12 +1,14 @@
-
-import requests,pydoc,webbrowser
-
+import requests,pydoc,webbrowser,sys,re
 #sprint(pydoc.__doc__)
   
 #print("Using __doc__:")
 #print(requests.__doc__)
+
+search="requests"
+match = re.search(r'requests', search)
+#search=str(search)
 res=requests.__doc__
-print(res)
+#print(res)
 #print(my_function.__doc__)
 f = open('C:/Users/VC/Desktop/PydocWebInterface/sample.html','w')
 
@@ -15,10 +17,18 @@ message = """
 	<head></head>
 	<body>
 		<input type="text" id="str" placeholder="Search string"/>
-		<input type="button" id="search" value="Search"/>
+		<input type="button" id="search" value="Search" onclick="msg()"/>
 	</body>
+	<script>
+	function msg(){
+		let search=document.getElementById("str").value;
+		console.log(search)
+		console.log({{res}})
+	}
+	</script>
 </html>
 """
 
 f.write(message)
 f.close()
+
